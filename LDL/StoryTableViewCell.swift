32fc54cc060081fefdb46638ce946dde939a1600
bryansum <bsummersett@gmail.com>
2016-12-09 01:56:55 +0100

    First commit; working on table view

---
+++ LDL/StoryTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  StoryTableViewCell.swift
+//  LDL
+//
+//  Created by Bryan Summersett on 12/9/16.
+//  Copyright © 2016 Bryan Summersett. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StoryTableViewCell: UITableViewCell {
+  @IBOutlet var titleLabel: UILabel!
+}
